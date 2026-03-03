ec9a0630249c1b905df58fc918dc746d8fcf1130
naveenthammu <naveenn610@gmail.com>
2026-03-03 13:15:40 +0000

    commiting file

---
+++ EmployeeManagementSystem/MyApp.API/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+
+using Microsoft.EntityFrameworkCore;
+using MyApp.API.Models;
+
+namespace MyApp.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
