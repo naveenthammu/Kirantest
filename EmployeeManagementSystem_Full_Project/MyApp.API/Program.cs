c5a4c72c1e4aff4b2e73509b551796f1158571a5
naveenthammu <naveenn610@gmail.com>
2026-03-04 10:04:10 +0000

    test cors

--- EmployeeManagementSystem_Full_Project/MyApp.API/Program.cs
+++ EmployeeManagementSystem_Full_Project/MyApp.API/Program.cs
@@ -1,16 +1,18 @@
-
 using Microsoft.EntityFrameworkCore;
 using MyApp.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// CORS configuration
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReact",
@@ -22,8 +24,10 @@ builder.Services.AddCors(options =>
         });
 });
 
-app.UseCors("AllowReact");
 var app = builder.Build();
+
+// Enable middleware
+app.UseCors("AllowReact");
 
 app.UseSwagger();
 app.UseSwaggerUI();
